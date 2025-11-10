6573a33197e3aa14116f10d52a893d32203c226d
Gauri <gauriagrawal@tamu.edu>
2025-11-10 15:10:28 -0600

    deployment attempt

--- backend/manager_menu.js
+++ backend/manager_menu.js
@@ -128,10 +128,12 @@ app.get('/api/orders/receipt/:receiptId', orderCtrl.getOrderByReceipt);
 app.get('/api/sales', orderCtrl.getSales);
 
 // Serve frontend
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 });
+
 
 // Start server
 app.listen(PORT, () => {
