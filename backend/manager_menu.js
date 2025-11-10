e5df1480dd2e0901c27720ed031f888e3f54e6e0
Gauri <gauriagrawal@tamu.edu>
2025-11-10 15:17:25 -0600

    deploy 3

--- backend/manager_menu.js
+++ backend/manager_menu.js
@@ -129,7 +129,7 @@ app.get('/api/sales', orderCtrl.getSales);
 
 // Serve frontend
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
-app.get('/*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 });
 
